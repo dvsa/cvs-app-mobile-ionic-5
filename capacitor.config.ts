66cda0052ddd0198bfcfd8b0b821f904ca12eb9b
Matt Culliford <mculliford@aol.com>
2021-10-26 16:40:14 +0100

    initial check in

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'uk.gov.dvsa.cvsmobileapp',
+  appName: 'Vehicle Testing',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
