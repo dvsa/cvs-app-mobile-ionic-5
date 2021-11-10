1d5e9a09985918e1ac6989c4baceb24c9f372312
Matthew Culliford <matthew.culliford@dvsa.gov.uk>
2021-11-10 10:49:29 +0000

    log in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Platform} from '@ionic/angular';
+import {NetworkService} from '@providers/global';
+import {AuthenticationService} from '@providers/auth';
+import {StorageService} from '@providers/natives/storage.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  constructor(
+    private platform: Platform,
+    private networkService: NetworkService,
+    private authenticationService: AuthenticationService,
+    public storageService: StorageService,
+  ) {
+  }
+
+  async ngOnInit() {
+    await this.platform.ready();
+    alert('ready');
+    await this.initApp();
+  }
+
+  async initApp() {
+
+    this.networkService.initialiseNetworkStatus();
+
+    await this.authenticationService.expireTokens();
+    //
+    // await this.appService.manageAppInit();
+    //
+    // const netWorkStatus: CONNECTION_STATUS = this.networkService.getNetworkState();
+    //
+    // if (netWorkStatus === CONNECTION_STATUS.OFFLINE) {
+    //   this.manageAppState();
+    //   return;
+    // }
+    //
+    const authStatus = await this.authenticationService.checkUserAuthStatus();
+    alert(authStatus);
+    // authStatus && !this.appService.isSignatureRegistered
+    //   ? this.navigateToSignaturePage()
+    //   : this.manageAppState();
+    //
+    // this.setupLogNetworkStatus();
+    //
+    // if (authStatus && this.appService.isCordova) {
+    //   await this.activateNativeFeatures();
+    // }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,36 +22,74 @@ import {PreparerService} from '@providers/preparer/preparer.service';
 import {CommonFunctionsService} from '@providers/utils/common-functions';
 import {LogsProvider} from '@store/logs/logs.service';
 import {SignatureService} from '@providers/signature/signature.service';
+import { Network } from '@ionic-native/network/ngx';
+import {SecureStorage} from '@ionic-native/secure-storage/ngx';
+import {HttpClientModule} from '@angular/common/http';
+import { IonicStorageModule } from '@ionic/storage-angular';
+import {LogsModule} from '@store/logs/logs.module';
+import {EffectsModule} from '@ngrx/effects';
+import {StoreModule} from '@ngrx/store';
+import {DataStoreProvider} from '@store/logs/data-store.service';
+
+const IONIC_NATIVE_PROVIDERS = [
+  // StatusBar,
+  // SplashScreen,
+  // SocialSharing,
+  // Camera,
+  // CallNumber,
+  // OpenNativeSettings,
+  // WheelSelector,
+  // MobileAccessibility,
+  // AppVersion,
+  // Keyboard,
+  // ScreenOrientation,
+  // GoogleAnalytics,
+  SecureStorage,
+  Network,
+];
 
 const CUSTOM_PROVIDERS = [
-  AppService,
-  AppAlertService,
-  SyncService,
+  // AppService,
+  // AppAlertService,
+  // SyncService,
   HTTPService,
   StorageService,
   AuthenticationService,
   VaultService,
   BrowserAuthPlugin,
   BrowserAuthService,
-  PreparerService,
-  VisitService,
-  ActivityService,
-  StateReformingService,
+  // PreparerService,
+  // VisitService,
+  // ActivityService,
+  // StateReformingService,
   CommonFunctionsService,
   LogsProvider,
   SignatureService,
-  AnalyticsService,
-  DurationService,
-  NetworkService
+  // AnalyticsService,
+  // DurationService,
+  NetworkService,
+  DataStoreProvider,
 ];
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule,
+    IonicStorageModule.forRoot({
+      driverOrder: ['sqlite', 'websql', 'indexeddb']
+    }),
+    LogsModule,
+    EffectsModule.forRoot([]),
+    StoreModule.forRoot({}),
+  ],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     ...CUSTOM_PROVIDERS,
+    ...IONIC_NATIVE_PROVIDERS,
   ],
   bootstrap: [AppComponent],
 })
