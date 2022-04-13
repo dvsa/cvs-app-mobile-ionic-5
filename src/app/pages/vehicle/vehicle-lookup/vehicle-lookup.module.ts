ff43a7071293744eb7d2b9eec694683bc7e52d51
Tom <thomas.evans@dvsa.gov.uk>
2022-04-13 11:19:13 +0100

    feat(562): pulled page in

---
+++ src/app/pages/vehicle/vehicle-lookup/vehicle-lookup.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { VehicleLookupPage } from './vehicle-lookup';
+import { VehicleService } from '../../../../providers/vehicle/vehicle.service';
+
+@NgModule({
+  declarations: [VehicleLookupPage],
+  imports: [IonicPageModule.forChild(VehicleLookupPage)],
+  providers: [VehicleService]
+})
+export class VehicleLookupModule {}
