e52ca14545d9a655daac3e93400d4783aee12200
Brandon Thomas-Davies <87308252+BrandonT95@users.noreply.github.com>
2022-06-01 11:48:43 +0100

    feat(feature/vta-629): fixed unit tests

--- src/app/pages/testing/test-creation/eu-vehicle-category/eu-vehicle-category.spec.ts
+++ src/app/pages/testing/test-creation/eu-vehicle-category/eu-vehicle-category.spec.ts
@@ -1,79 +1,78 @@
-// import { CategoryReadingPage } from './eu-vehicle-category';
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-// import { IonicModule, NavParams, ViewController } from 'ionic-angular';
-// import { NavParamsMock } from '../../../../../test-config/ionic-mocks/nav-params.mock';
-// import { VisitService } from '../../../../providers/visit/visit.service';
-// import { VisitServiceMock } from '../../../../../test-config/services-mocks/visit-service.mock';
-// import { ViewControllerMock } from '../../../../../test-config/ionic-mocks/view-controller.mock';
-// import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-// import { EuVehicleCategoryData } from '../../../../assets/app-data/eu-vehicle-category/eu-vehicle-category';
-// import { VehicleModel } from '../../../../models/vehicle/vehicle.model';
-// import { VehicleDataMock } from '../../../../assets/data-mocks/vehicle-data.mock';
-// import { VehicleService } from '../../../../providers/vehicle/vehicle.service';
-// import { VehicleServiceMock } from '../../../../../test-config/services-mocks/vehicle-service.mock';
-//
-// describe('Component: EuVehicleCategoryPage', () => {
-//   let component: CategoryReadingPage;
-//   let fixture: ComponentFixture<CategoryReadingPage>;
-//
-//   const VEHICLE_DATA: VehicleModel = VehicleDataMock.VehicleData;
-//
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [CategoryReadingPage],
-//       imports: [IonicModule.forRoot(CategoryReadingPage)],
-//       providers: [
-//         { provide: NavParams, useClass: NavParamsMock },
-//         { provide: VisitService, useClass: VisitServiceMock },
-//         { provide: ViewController, useClass: ViewControllerMock },
-//         { provide: VehicleService, useClass: VehicleServiceMock }
-//       ],
-//       schemas: [CUSTOM_ELEMENTS_SCHEMA]
-//     }).compileComponents();
-//   }));
-//
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(CategoryReadingPage);
-//     component = fixture.componentInstance;
-//   });
-//
-//   afterEach(() => {
-//     fixture.destroy();
-//     component = null;
-//   });
-//
-//   it('should create the component', () => {
-//     expect(fixture).toBeTruthy();
-//     expect(component).toBeTruthy();
-//   });
-//
-//   it('should test ngOnInit', () => {
-//     component.vehicle = VehicleDataMock.VehicleData;
-//     component.vehicle.techRecord.vehicleType = 'psv';
-//     component.ngOnInit();
-//     expect(component.categorySubtitle).toEqual(EuVehicleCategoryData.EuCategoryPsvSubtitleData);
-//     component.vehicle.techRecord.vehicleType = 'hgv';
-//     component.ngOnInit();
-//     expect(component.categorySubtitle).toEqual(EuVehicleCategoryData.EuCategoryHgvSubtitleData);
-//     component.vehicle.techRecord.vehicleType = 'trl';
-//     component.ngOnInit();
-//     expect(component.categorySubtitle).toEqual(EuVehicleCategoryData.EuCategoryTrlSubtitleData);
-//   });
-//
-//   it('should test errorIncomplete logic', () => {
-//     component.errorIncomplete = true;
-//     component.vehicle = VEHICLE_DATA;
-//     component.vehicle.euVehicleCategory = '';
-//     component.ngOnInit();
-//     expect(component.errorIncomplete).toBeTruthy();
-//     component.vehicle.euVehicleCategory = 'm1';
-//     component.ngOnInit();
-//     expect(component.errorIncomplete).toBeFalsy();
-//   });
-//
-//   it('should test setVehicleCategory logic', () => {
-//     component.vehicle = VEHICLE_DATA;
-//     component.setVehicleCategory('m1');
-//     expect(component.vehicle.euVehicleCategory).toEqual('m1');
-//   });
-// });
+import { CategoryReadingPage } from './eu-vehicle-category';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { VisitService } from '@providers/visit/visit.service';
+import { VisitServiceMock } from '@test-config/services-mocks/visit-service.mock';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { EuVehicleCategoryData } from '@assets/app-data/eu-vehicle-category/eu-vehicle-category';
+import { VehicleModel } from '@models/vehicle/vehicle.model';
+import { VehicleDataMock } from '@assets/data-mocks/vehicle-data.mock';
+import { VehicleService } from '@providers/vehicle/vehicle.service';
+import { VehicleServiceMock } from '@test-config/services-mocks/vehicle-service.mock';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ModalController } from '@ionic/angular';
+import { ModalControllerMock } from 'ionic-mocks';
+
+describe('Component: EuVehicleCategoryPage', () => {
+  let component: CategoryReadingPage;
+  let fixture: ComponentFixture<CategoryReadingPage>;
+
+  const VEHICLE_DATA: VehicleModel = VehicleDataMock.VehicleData;
+
+  beforeEach( waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [CategoryReadingPage],
+      imports: [RouterTestingModule.withRoutes([])],
+      providers: [
+        { provide: VisitService, useClass: VisitServiceMock },
+        { provide: ModalController, useFactory: () => ModalControllerMock.instance() },
+        { provide: VehicleService, useClass: VehicleServiceMock }
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
+    });
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CategoryReadingPage);
+    component = fixture.componentInstance;
+  });
+
+  afterEach(() => {
+    fixture.destroy();
+    component = null;
+  });
+
+  it('should create the component', () => {
+    expect(fixture).toBeTruthy();
+    expect(component).toBeTruthy();
+  });
+
+  it('should test ngOnInit', () => {
+    component.vehicle = VehicleDataMock.VehicleData;
+    component.vehicle.techRecord.vehicleType = 'psv';
+    component.ngOnInit();
+    expect(component.categorySubtitle).toEqual(EuVehicleCategoryData.EuCategoryPsvSubtitleData);
+    component.vehicle.techRecord.vehicleType = 'hgv';
+    component.ngOnInit();
+    expect(component.categorySubtitle).toEqual(EuVehicleCategoryData.EuCategoryHgvSubtitleData);
+    component.vehicle.techRecord.vehicleType = 'trl';
+    component.ngOnInit();
+    expect(component.categorySubtitle).toEqual(EuVehicleCategoryData.EuCategoryTrlSubtitleData);
+  });
+
+  it('should test errorIncomplete logic', () => {
+    component.errorIncomplete = true;
+    component.vehicle = VEHICLE_DATA;
+    component.vehicle.euVehicleCategory = '';
+    component.ngOnInit();
+    expect(component.errorIncomplete).toBeTruthy();
+    component.vehicle.euVehicleCategory = 'm1';
+    component.ngOnInit();
+    expect(component.errorIncomplete).toBeFalsy();
+  });
+
+  it('should test setVehicleCategory logic', () => {
+    component.vehicle = VEHICLE_DATA;
+    component.setVehicleCategory('m1');
+    expect(component.vehicle.euVehicleCategory).toEqual('m1');
+  });
+});
